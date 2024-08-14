95e6d1d2a6811c98672d5fa570e98edf7ee9d39e
Aayush Gauutam <professional.atus6693@gmail.com>
2024-08-14 08:36:07 +0545

    simple get req

--- express/index.js
+++ express/index.js
@@ -1,15 +1,30 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
 // routing
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from express server");
-});
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello from express server");
+// });
 
-app.get("/index", (req, res) => {
-  res.status(200).send("Hello from express server index");
+// app.get("/index", (req, res) => {
+//   res.status(200).send("Hello from express server index");
+// });
+
+const { tours } = JSON.parse(fs.readFileSync("sample.json"));
+
+app.get("/api/v1/tours", (req, res) => {
+  console.log("tttt", tours.length);
+  res.status(200).json({
+    // halka formatting
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 const port = 3000;
