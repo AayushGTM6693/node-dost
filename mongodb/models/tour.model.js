87def72d5436d201b7c6fa0629e21490bbe6bc2f
Aayush Gauutam <professional.atus6693@gmail.com>
2024-08-20 22:05:53 +0545

    refactor: Update tour model schema with additional fields and validation

--- mongodb/models/tour.model.js
+++ mongodb/models/tour.model.js
@@ -4,15 +4,54 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 4.5,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupsize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a group size"],
+  },
+
+  ratingsAverage: {
+    type: Number,
+    default: 1.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, "A tour must have a price"],
   },
+
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // remove whitespaces at start and end
+    required: [true, "A tour must have a description"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 // every thing that isnt in the schema is ignored from the post req
 
